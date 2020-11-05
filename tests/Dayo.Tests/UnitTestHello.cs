bf9c771a25157aba9b7c08554c244d3e2a05073d
Geralt Baturo <gdmaster113.live@gmail.com>
2020-11-05 21:53:11 +0100

    Added test project with first test

---
+++ tests/Dayo.Tests/UnitTestHello.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dayo.Tests
+{
+    public class UnitTestHello
+    {
+        [Fact]
+        public void Should_run_test_succesfully()
+        {
+            Assert.True(true);
+        }
+    }
+}
