0cbd1d83add7ff967d1abef9cdedc09ddf0be0b9
Paweł Baturo <pbaturo@teldat.local>
2020-11-22 22:34:51 +0100

    Added SimpleInjector container. Refactred to use depenencies through IOC.

--- src/Dayo/App.xaml.cs
+++ src/Dayo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SimpleInjector;
 
 namespace Dayo
 {
@@ -13,12 +14,25 @@ namespace Dayo
     /// </summary>
     public partial class App : Application
     {
+        private Container InitilizeContainer() 
+        {
+            Container container = new Container();
+
+            container.Register<Store>(Lifestyle.Singleton);
+            container.Register<MainWindowViewModel>();
+            container.Register<MainWindow>();
+
+            container.Verify();
+
+            return container;
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
 		{
-            Store store = new Store();
-            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel(store);
-			MainWindow mainWindow = new MainWindow(mainWindowViewModel, store);
-			mainWindow.Show();
+            
+            Container container = InitilizeContainer();
+            MainWindow mainWindow = container.GetInstance<MainWindow>();
+            mainWindow.Show();
 		}
     }
 }
